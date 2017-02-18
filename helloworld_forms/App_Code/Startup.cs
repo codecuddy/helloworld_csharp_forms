fb1589f8c456e21a810c89d8685b7bd265698fdb
Charlie Cuddy <charliecuddy4@gmail.com>
2017-02-18 17:22:42 -0600

    add time

---
+++ helloworld_forms/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(helloworld_forms.Startup))]
+namespace helloworld_forms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
