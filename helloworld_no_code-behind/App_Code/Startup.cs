67ae5ed666ad0dfa384d7042686278c90fb21c46
Charlie Cuddy <charliecuddy4@gmail.com>
2017-02-18 21:19:47 -0600

    initial commit

---
+++ helloworld_no_code-behind/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(helloworld_no_code_behind.Startup))]
+namespace helloworld_no_code_behind
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
